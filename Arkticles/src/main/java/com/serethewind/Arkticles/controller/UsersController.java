b49449edb01e63dc32e1c1dd625314b942d46479
serethewind <osasereu@gmail.com>
2023-06-07 23:17:03 +0100

    CRUD operations effected in user controller

--- Arkticles/src/main/java/com/serethewind/Arkticles/controller/UsersController.java
+++ Arkticles/src/main/java/com/serethewind/Arkticles/controller/UsersController.java
@@ -1,9 +1,43 @@
 package com.serethewind.Arkticles.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.serethewind.Arkticles.dto.users.UserRequestDto;
+import com.serethewind.Arkticles.dto.users.UserResponseDto;
+import com.serethewind.Arkticles.service.users.serviceImpl.UsersServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/arkticles/v1/users")
+@AllArgsConstructor
 public class UsersController {
+
+    private UsersServiceImpl usersService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> getUsers(){
+        return ResponseEntity.ok(usersService.viewAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getSingleUser(@PathVariable("id") Long userId){
+        return ResponseEntity.ok(usersService.viewSingleUser(userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto userRequestDto){
+        return ResponseEntity.ok(usersService.createUser(userRequestDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUserDetails(@PathVariable("id") Long id, @RequestBody UserRequestDto userRequestDto){
+        return ResponseEntity.ok(usersService.updateUserInformation(id, userRequestDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId){
+        return ResponseEntity.ok(usersService.deleteUser(userId));
+    }
 }
