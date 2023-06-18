b84ef9ea66ef887634dbe30fc1d5f8489aa39404
serethewind <osasereu@gmail.com>
2023-06-18 21:22:43 +0100

    JWt Authentication filter is made aware of the need to validate that token is not expired nor revoked in the token respository

--- Arkticles/src/main/java/com/serethewind/Arkticles/securityConfig/JwtAuthenticationFilter.java
+++ Arkticles/src/main/java/com/serethewind/Arkticles/securityConfig/JwtAuthenticationFilter.java
@@ -1,5 +1,8 @@
 package com.serethewind.Arkticles.securityConfig;
 
+import com.serethewind.Arkticles.entity.TokenEntity;
+import com.serethewind.Arkticles.exceptions.ResourceNotFoundException;
+import com.serethewind.Arkticles.repository.TokenRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 
 //@AllArgsConstructor
@@ -21,7 +25,7 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     /**
-     * the request, the response, consider filterchain like a pointer to the next filter in the chain
+     * the request, the response, consider filter chain like a pointer to the next filter in the chain
      * extract the authorization details from the request.
      * check whether the details is null or does not start with bearer.
      * if either of the above conditions, go to the next filter in the chain.
@@ -40,6 +44,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private JWTService jwtService;
     private CustomUserDetailsService userDetailsService;
+    private TokenRepository tokenRepository;
 
     @Override
     protected void doFilterInternal(
@@ -56,12 +61,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         String jwt = authHeader.substring(7);
-        System.out.println(jwt);
         String username = jwtService.getUsername(jwt);
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null){
            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-           if (jwtService.isTokenValid(jwt, userDetails)){
+
+          Boolean isValid = tokenRepository.findByToken(jwt).map(t -> !t.getRevoked() && !t.getExpired()).orElse(false);
+
+
+           if (jwtService.isTokenValid(jwt, userDetails) && isValid){
                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                authenticationToken.setDetails(new WebAuthenticationDetailsSource());
                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
